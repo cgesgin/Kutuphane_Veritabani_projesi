9c5a52105c4529eea7f0caa3b0a2916c649d517c
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2021-02-05 23:58:26 +0300

    Update 01_query.sql

--- sql-kodlar/01_query.sql
+++ sql-kodlar/01_query.sql
@@ -20,3 +20,36 @@ create table tbl_emanet(
 	emanet_tarihi datetime,
 	teslim_tarihi datetime,
 );
+
+create table tbl_yazarlar(
+	yazar_no int identity(1,1) primary key not null,
+	yazar_adi varchar(20),
+	yazar_soyadi varchar(20)
+);
+
+create table tbl_kutuphane(
+	kutuphane_no int identity(1,1) primary key not null,
+	kutuphane_ismi varchar(20),
+	aciklama varchar(20),
+	adres_no int
+);
+
+create table tbl_kitap_kategori(
+	kitap_kategori_id int identity(1,1),
+	isbn int not null ,
+	kategori_no int not null, 
+	constraint "ktp_ktg_pk"
+		primary key("kitap_kategori_id","isbn","kategori_no")
+);
+
+create table tbl_adresler(
+	adres_no int identity(1,1) primary key,
+	cadde varchar(20),
+	mahalle varchar(20),
+	bina_no int,
+	sehir varchar(20),
+	posta_kodu int,
+	ulke varchar(20)	
+);
+
+
