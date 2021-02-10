e083bc4de16823f89ea6d39bc018706acbe0bb3c
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2021-02-10 15:22:04 +0300

    Update otoGaleriDB.sql

--- extralar-calışmalar/otoGaleriDB.sql
+++ extralar-calışmalar/otoGaleriDB.sql
@@ -3,7 +3,7 @@
 
 create database db_otogaleri;
 
---Tablo tanımlamaları 
+				--Tablo tanımlamaları 
 
 create table tbl_arac(
 	arac_no int identity(1,1) primary key,
@@ -40,7 +40,7 @@ create table tbl_alım(
 	fiyat int
 );
 
---Tablolarda constraint işlemleri
+			--Tablolarda constraint işlemleri
 
 alter table tbl_satıs add constraint musteri_satis_fk 
 	foreign key (musteri_no) references tbl_musteri(musteri_no);
@@ -72,7 +72,8 @@ alter table tbl_satıs add constraint df_satis_tarihi default '01.01.1900' for s
 
 alter table tbl_alım add constraint df_alim_tarihi default '01.01.1900' for alim_tarih;
 
---Tablolara veri girişleri		       
+
+		       --Tablolara veri girişleri		       
 		       
 insert into tbl_musteri values('turgut','özseven','meram/konya',12345);
 insert into tbl_musteri values('mustafa','çağlayan','meram/konya',99345);
@@ -110,7 +111,8 @@ insert into tbl_alım values(9,29,'02.07.2010',26000);
 ---drop table tbl_musteri;
 ---drop table tbl_arac;
 
---Tek tablo üzerinden sorgulamalar
+
+		       --Tek tablo üzerinden sorgulamalar
 		       
 select model,marka,fiyat from tbl_arac where model<=2004;
 select model,marka,fiyat from tbl_arac where model>=2000 and model<=2900;
@@ -132,5 +134,23 @@ select adres,telefon from tbl_musteri where ad like'%A%';
 select model,marka,fiyat from tbl_arac where marka in('Opel','Fiat') order by marka asc;
 select * from tbl_arac where model in(1998,2005,2007);
 
+		       --Aritmetik operatorler ve fonksiyonlar üzerinden işlemler
 
-
+ select *,fiyat*20/100 as "%20 arrtılmıs fiyatlar"  from tbl_arac;
+ select *,DATEDIFF(YEAR,model,getdate()) as "yeni tarih" from tbl_arac where DATEDIFF(YEAR,model,getdate())<=3;
+ select ad+soyad as "Ad Soyad" from tbl_musteri order by [Ad Soyad];
+ select upper(SUBSTRING(ad,1,1))+'.'+soyad as adSoyad from tbl_musteri;
+ select * from tbl_alım order by fiyat desc,datename(month,alim_tarih) asc;
+ select UPPER(SUBSTRING(ad,1,1))+SUBSTRING(soyad,1,5) as "ad soyad" from tbl_musteri where LEN(soyad)>5;
+ select SUBSTRING(adres,CHARINDEX('/',adres)+1,LEN(adres))as "il" from tbl_musteri order by il asc;
+ select * from tbl_satıs where (datepart(YEAR,satis_tarih)=2008 or DATEPART(YEAR,satis_tarih)=2010) and (3=DATEPART(MONTH,satis_tarih) or 4=DATEPART(MONTH,satis_tarih));
+ select sum(fiyat)as "2008 ve 2010 yılları arası 4. ve 3. ay toplam fiyat" from tbl_satıs where (datepart(YEAR,satis_tarih)=2008 or DATEPART(YEAR,satis_tarih)=2010) and (3=DATEPART(MONTH,satis_tarih) or 4=DATEPART(MONTH,satis_tarih));
+ select sum(fiyat)as "fiyat",AVG(fiyat)as "orta",(sum(fiyat)-AVG(fiyat)) as"toplam-ortalama=fark" from tbl_satıs where (datepart(YEAR,satis_tarih)=2008 or DATEPART(YEAR,satis_tarih)=2010) and (1=DATEPART(MONTH,satis_tarih) or 8=DATEPART(MONTH,satis_tarih));
+ select model,marka,fiyat from tbl_arac where fiyat=(select  max(fiyat)  from tbl_arac );
+ select  max(fiyat)-MIN(fiyat)  from tbl_arac ;
+ select * from tbl_arac where fiyat<=(select  max(fiyat)-MIN(fiyat)  from tbl_arac) ;
+ select COUNT(*) from tbl_alım where DATEPART(YEAR,alim_tarih)=2010;
+ select * from tbl_satıs where satis_tarih between  '2010.03.01'   and  '2010.12.31' ;
+ select * from tbl_alım where 6>DATEDIFF(MONTH,alim_tarih,GETDATE());
+ select datename(WEEKDAY,satis_tarih)as "satılan günler" , datename(DAY,satis_tarih) from tbl_satıs;
+		   
