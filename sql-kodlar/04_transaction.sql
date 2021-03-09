51b2a5558337f974b2ae46668e2ce672fd3277e8
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2021-03-09 12:03:36 +0300

    Update 04_transaction.sql

--- sql-kodlar/04_transaction.sql
+++ sql-kodlar/04_transaction.sql
@@ -12,6 +12,13 @@ SELECT * fROM tbl_kitaplar
 --Eğer transaction işlemi ROLLBACK yerine COIMMIT ile bitirilirse yapılan değişikliklerin
 --tamamı kalıcı olarak veritabanına aktarılacaktır.
 
+BEGIN transaction--yapılan işleme butunluk sağlamak
+UPDATE tbl_kitaplar SET sayfa_sayisi=0
+UPDATE tbl_kitaplar SET kitap_adi='Deneme'
+SELECT * FROM tbl_kitaplar
+commit--kalıcı olarak veri tabanına veri aktarıldı
+SELECT * fROM tbl_kitaplar
+
 --Uzun transaction işlemleri için transaction kendi içerisinde savepoints adı verilen küçük
 --bölümlere ayniabiiir. Her bir bölüm commit ile veritabanına transfer edilebilir veya
 --rollback ile yapılan değişiklik geri alınabilir.
